61e2cf15d5e3e67ff8430749ab88c5473978b8e9
unknown <buj4r.hyseni@gmail.com>
2023-11-03 09:33:06 +0100

    Client Url changed

--- server/server.js
+++ server/server.js
@@ -34,14 +34,14 @@ app.use(cors());
 
 // Enable CORS for all routes
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://soc-net123.netlify.app/");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://soc-net123.netlify.app/");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
